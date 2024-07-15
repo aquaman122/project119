a1c5cd3800e8a33ffcf4cbf8b32bd502bf158fa9
aquaman122 <swj981128@gmail.com>
2024-07-15 17:13:36 +0900

    feat/react-typescript

--- client/src/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "./store";
-
-// 일반적인 `useDispatch`와 `useSelector` 대신에 사용
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
